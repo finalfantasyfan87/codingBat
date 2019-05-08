88d4a9415b3c352569cf64c349c53214d3a5c138
jennifer.haywood-at-262472638887 <jennifer.haywood@bytecurve.com>
2019-05-08 10:51:49 -0500

    no message

--- src/codingbat/functional1/Functional1.java
+++ src/codingbat/functional1/Functional1.java
@@ -56,4 +56,30 @@ copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
         return somelist;
     }
 
+        /*
+Functional-1 > moreY
+Given a list of strings, return a list where each string has "y" added at its start and end.
+        moreY(["a", "b", "c"]) → ["yay", "yby", "ycy"]
+moreY(["hello", "there"]) → ["yhelloy", "ytherey"]
+moreY(["yay"]) → ["yyayy"]
+         */
+        public List<String> moreY(List<String> strings) {
+            List<String> somelist = new ArrayList<>();
+            strings.forEach(s -> somelist.add("y".concat(s).concat("y")));
+            return somelist;
+        }
+
+        /*
+        Functional-1 > math1
+Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+math1([1, 2, 3]) → [20, 30, 40]
+math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+math1([10]) → [110]
+         */
+        public List<Integer> math1(List<Integer> nums) {
+            List<Integer> numsList = nums.stream().map(num-> 10*(num+1)).collect(Collectors.toList());
+            return numsList;
+
+        }
+
 }
