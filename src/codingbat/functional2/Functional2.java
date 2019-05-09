1f894f00b505082227f42bc0c401be55fc0cce24
jennifer.haywood-at-262472638887 <jennifer.haywood@bytecurve.com>
2019-05-09 11:45:49 -0500

    no message

--- src/codingbat/functional2/Functional2.java
+++ src/codingbat/functional2/Functional2.java
@@ -39,4 +39,30 @@ noTeen([15]) → []
         return nums.stream().filter(n->!( n>=13 && n<=19)).collect(Collectors.toList());
     }
 
+    /*
+
+Given a list of strings, return a list of the strings, omitting any string that contains a "z". (Note: the str.contains(x) method returns a boolean)
+
+
+noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+noZ(["hziz", "hzello", "hi"]) → ["hi"]
+noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+     */
+    public List<String> noZ(List<String> strings) {
+        return strings.stream().filter(s -> !(s.contains("z"))).collect(Collectors.toList());
+    }
+    /*
+
+Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+
+
+noLong(["this", "not", "too", "long"]) → ["not", "too"]
+noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+noLong(["cccc", "cccc", "cccc"]) → []
+     */
+
+    public List<String> noLong(List<String> strings) {
+        return strings.stream().filter(s -> !(s.length() >4)).collect(Collectors.toList());
+    }
+
 }
