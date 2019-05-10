1124a2a0fe9851f0e1423b2e686aa63dd143fbb5
jennifer.haywood-at-262472638887 <jennifer.haywood@bytecurve.com>
2019-05-10 16:12:31 -0500

    wc

--- src/codingbat/recursion1/Recursion1.java
+++ src/codingbat/recursion1/Recursion1.java
@@ -114,6 +114,31 @@ sumDigits(12) → 3
     }
 
 
+    /*
+
+Given a non-negative int n, return the count of the occurrences of 7 as a digit,
+ so for example 717 yields 2. (no loops). Note that mod (%)
+ by 10 yields the rightmost digit (126 % 10 is 6), while divide (/)
+ by 10 removes the rightmost digit (126 / 10 is 12).
+count7(717) → 2
+count7(7) → 1
+count7(123) → 0
+     */
+    public int count7(int n) {
+        int occurences =0;
+        if (n == 0) {
+            return 0;
+        }
+        if (n < 1 || n ==7) {
+            return 1;
+        }
+        if(){
+            occurences =
+            return
+        }
+    }
+
+
 }
 
 
